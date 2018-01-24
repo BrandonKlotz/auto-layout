5951afd2c7546117d83546534dd7169db7e90111
Brandon Klotz <brandonklotz@icloud.com>
2018-01-24 09:27:59 -0500

    Initial Commit

---
+++ auto-layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auto-layout
+//
+//  Created by Brandon Klotz on 1/24/18.
+//  Copyright © 2018 Brandon Klotz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
